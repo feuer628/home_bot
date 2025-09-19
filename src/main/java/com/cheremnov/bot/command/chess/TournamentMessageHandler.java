373168cad7ec692fe0ae391604c77e8da5a05cb9
Cheremnov <cheremnov@bifit.com>
2025-09-19 18:12:11 +0300

    Одновременное добавление нескольких групп

--- src/main/java/com/cheremnov/bot/command/chess/TournamentMessageHandler.java
+++ src/main/java/com/cheremnov/bot/command/chess/TournamentMessageHandler.java
@@ -2,9 +2,14 @@ package com.cheremnov.bot.command.chess;
 
 import com.cheremnov.bot.Bot;
 import com.cheremnov.bot.command.AbstractMessageHandler;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Message;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @Component
 public class TournamentMessageHandler extends AbstractMessageHandler {
@@ -14,18 +19,34 @@ public class TournamentMessageHandler extends AbstractMessageHandler {
 
     @Override
     public boolean handleMessage(Message message, Bot bot) {
-        String text = message.getText();
-        String[] tournament = text.split("-");
-        if (tournament.length != 2) {
+
+        List<String> tours = List.of(message.getText().split("\n"));
+        AtomicBoolean isError = new AtomicBoolean(false);
+        List<GroupTour> groupTours = new ArrayList<>();
+        tours.forEach(tour -> {
+            String[] tournament = tour.split("-");
+            if (tournament.length != 2 || StringUtils.isBlank(tournament[0]) || StringUtils.isBlank(tournament[1])) {
+                isError.set(true);
+                return;
+            }
+            int tourCount = Integer.parseInt(tournament[1].trim());
+            GroupTour groupTour = new GroupTour();
+            groupTour.setTourCount(tourCount);
+            groupTour.setName(tournament[0].trim());
+            groupTours.add(groupTour);
+        });
+        if (isError.get()) {
             bot.sendText(message.getChatId(), "Неверный формат сообщения");
             return false;
         }
-        int tourCount = Integer.parseInt(tournament[1].trim());
-        GroupTour groupTour = new GroupTour();
-        groupTour.setTourCount(tourCount);
-        groupTour.setName(tournament[0].trim());
-        groupRepository.save(groupTour);
-        bot.sendText(message.getChatId(), "Добавлена группа турнира: " + groupTour.getName());
+        StringBuilder textToSend = new StringBuilder("Добавлены группы турнира: ");
+        groupTours.forEach(groupTour -> {
+                    textToSend.append(groupTour.getName()).append("; ");
+                    groupRepository.save(groupTour);
+                }
+        );
+
+        bot.sendText(message.getChatId(), textToSend.toString().trim());
         return true;
     }
 }
