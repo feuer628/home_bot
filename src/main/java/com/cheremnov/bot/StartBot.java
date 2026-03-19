7193bb76107ee459d6e6817e28d9a2b3d0ca9b1b
Cheremnov <cheremnov@bifit.com>
2026-03-19 11:45:15 +0300

    Исправления, улучшения
    (cherry picked from commit 442eed3a719fb115e44e883a4c4d42923c88f08c)

--- src/main/java/com/cheremnov/bot/StartBot.java
+++ src/main/java/com/cheremnov/bot/StartBot.java
@@ -24,7 +24,6 @@ public class StartBot {
             telegramBotsApi.registerBot(bot);
         } catch (Exception e) {
             log.error("Ошибка при инициализации бота", e);
-            throw new RuntimeException(e);
         }
     }
 
