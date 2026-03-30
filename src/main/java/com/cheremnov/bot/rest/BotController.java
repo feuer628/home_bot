62eca524867e7b2d14f250ccbf305f65c077bc53
Cheremnov <cheremnov@bifit.com>
2026-03-30 09:00:11 +0300

    Добавил пост запрос для открытия калитки через Алису и Home Assistant

--- src/main/java/com/cheremnov/bot/rest/BotController.java
+++ src/main/java/com/cheremnov/bot/rest/BotController.java
@@ -1,10 +1,13 @@
 package com.cheremnov.bot.rest;
 
 import com.cheremnov.bot.Bot;
+import com.cheremnov.bot.command.door.TuyaAdapter;
 import com.cheremnov.bot.db.subscibers.SubscriberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,5 +28,13 @@ public class BotController {
             }
             bot.sendText(subscriber.getChatId(), "Ты есть - " + subscriber.getName());
         });
+    }
+
+    @PostMapping("/open-door")
+    public ResponseEntity<String> openDoor() {
+        log.info("Поступил запрос на открытие калитки");
+        TuyaAdapter.openDoor();
+
+        return ResponseEntity.ok("{\"status\":\"ok\"}");  // Явный 200 с JSON
     }
 }
